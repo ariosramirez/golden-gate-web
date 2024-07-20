6317f3bfb9d3f1bfc25cc19a650a979e5705c480
ariosramirez <andres@beek.io>
2024-07-20 08:29:55 -0300

    Improved the content marging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ImageGrid from './components/ImageGrid';
+import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header>
+      <header className="header">
         <h1>Golden Gate</h1>
       </header>
-      <main>
+      <main className="main-content main-container">
         <ImageGrid />
       </main>
-      {/*<footer>*/}
+      {/*<footer className="footer">*/}
       {/*  <p>&copy; 2024 Golden Gate</p>*/}
       {/*</footer>*/}
     </div>
